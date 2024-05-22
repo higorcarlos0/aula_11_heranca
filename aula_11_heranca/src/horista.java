ae26a67ab00db3cb5c2518d706c4b51635ed154b
higorcarlos0 <higorcarlos252@gmail.com>
2024-05-22 10:21:08 -0300

    meu segundo commit

--- aula_11_heranca/src/horista.java
+++ aula_11_heranca/src/horista.java
@@ -1,3 +1,24 @@
-public class horista {
+public class horista  extends empregado{
+    private double horas;
+
+    public double getHoras() {
+        return horas;
+    }
+
+    public void setHoras(double horas) {
+        this.horas = horas;
+    }
+
+    public horista(String nome, String endereço, Double salario, double horas) {
+        super(nome, endereço, salario);
+        this.horas = horas;
+    }
+
+    public horista(double horas) {
+        this.horas = horas;
+    }
     
+    public void calculoSalario (){
+        salario = horas * 50;
+    }
 }
